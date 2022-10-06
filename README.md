a0f551f3794157b44218d34a99198f45f0385484
Bohdan Cherniak <djsteward.rv@gmail.com>
2022-10-06 21:36:59 +0300

    Add README.md, add MSSQL server conf

---
+++ README.md
@@ -0,0 +1,25 @@
+# Dashboard application with using React, TypeScript, Redux, Express and MS SQL
+
+## Install node_modules for Front-End and Back-End
+
+In the project directory, run:
+
+### `npm i && npm i --prefix client-app`
+
+## Available Scripts
+
+Development mode:\
+
+### `npm run dev`
+
+Run server:\
+
+### `npm run start`
+
+Run React app:\
+
+### `npm run client`
+
+Run Seeder.js:\
+
+### `npm run seed`
--- data/config/settings.js
+++ data/config/settings.js
@@ -1,7 +1,20 @@
+// Use IT Step server
+
+// module.exports = Confgiration = {
+//   DATABASE_URL: "10.7.101.197",
+//   DATABASE_NAME: "f12",
+//   DATABASE_USER: "sa",
+//   DATABASE_PASS: "Qwerty-1",
+//   DATABASE_PORT: 1433,
+//   DATABASE_DIALECT: "mssql",
+// };
+
+// Use a server accessible to all
+
 module.exports = Confgiration = {
-  DATABASE_URL: "10.7.101.197",
-  DATABASE_NAME: "f12",
-  DATABASE_USER: "sa",
+  DATABASE_URL: "77.123.28.68",
+  DATABASE_NAME: "Dashboard",
+  DATABASE_USER: "f21",
   DATABASE_PASS: "Qwerty-1",
   DATABASE_PORT: 1433,
   DATABASE_DIALECT: "mssql",
